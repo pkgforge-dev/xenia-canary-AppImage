541fc1c9a485d44d33420d3f61d59a9a5b196c06
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-16 04:30:57 -0400

    simplify

--- xenia-appimage.sh
+++ xenia-appimage.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 ARCH="$(uname -m)"
 VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-
-export OUTPUT_APPIMAGE=1
+export ARCH VERSION
+export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export OUTNAME=xenia-canary-"$VERSION"-anylinux-"$ARCH".AppImage
 export ICON=https://raw.githubusercontent.com/xenia-canary/xenia-canary/refs/heads/canary_experimental/assets/icon/256.png
 export DEPLOY_VULKAN=1
 
 # ADD LIBRARIES
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun ./build/bin/Linux/Release/xenia_canary
+quick-sharun ./build/bin/Linux/Release/xenia_canary
 
-mkdir -p ./dist
-mv -v ./*.AppImage* ./dist
-mv -v ~/version     ./dist
+# Additional changes can be done in between here
+
+# Turn AppDir into AppImage
+quick-sharun --make-appimage
